0df0bd6b56dc0239617f4d6e61b7c5dde2b24ada
Azu-bit <126981765+Azu-bit@users.noreply.github.com>
2024-02-15 16:38:26 -0600

    Cambios realizados

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -9,12 +9,12 @@ package tarea2;
  * @author Manager
  */
 public class Tarea2 {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+System.out.println("Hola Mundo");        // TODO code application logic here
     }
     
 }
