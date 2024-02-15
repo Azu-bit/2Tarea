57162792209dafd007de1d182b604da3ba29ae09
Azu-bit <126981765+Azu-bit@users.noreply.github.com>
2024-02-15 16:59:38 -0600

    Cambios realizados otra vez

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -14,7 +14,9 @@ public class Tarea2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-System.out.println("Hola Mundo");        // TODO code application logic here
+System.out.println("Hola Mundo");
+int variable = 1;
+// TODO code application logic here
     }
     
 }
